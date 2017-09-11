fe8e994a01a2ad2714450b8061f20327a97bbb3b
botzhou <277168645@qq.com>
2017-09-11 08:31:03 +0800

    Update form.py

--- form.py
+++ form.py
@@ -3,7 +3,7 @@ from flask import request
 from flask import render_template
 from flask import redirect
 from models import *
-from wtforms import Form,PasswordField,SubmitField,StringField,BooleanField
+from wtforms import Form,PasswordField,SubmitField,StringField,BooleanField,RadioField
 from wtforms.validators import Required,Length,Email,Regexp,EqualTo
 from wtforms import validators
 
@@ -12,4 +12,7 @@ class RegistrationForm(Form):
     username = StringField('Username',validators=[Required(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
 'Usernames must have only letters, '
 'numbers, dots or underscores')])
-    password = PasswordField('Password',validators=[Required()])
+    password = PasswordField('请输入密码',validators=[Required(),EqualTo('password2',message='password must match')])
+    password2 = PasswordField('确认你的密码',validators= [Required()])
+    submit = SubmitField('Retister')
+    sex = RadioField('性别',choices=[('value','description'),('value_two','whatever')])
